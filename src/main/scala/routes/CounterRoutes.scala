65ddbfcab903c382366afcc8f2df3cefc1db9999
dlbdvd96 <dlbdvd96@hotmail.it>
2024-05-16 23:38:33 +0200

    Use direct style in CounterRoutes

--- src/main/scala/routes/CounterRoutes.scala
+++ src/main/scala/routes/CounterRoutes.scala
@@ -2,16 +2,16 @@ package routes
 
 import aliases.URRoutes
 import layers.CounterService
+import zio.direct.*
 import zio.http.*
 
 object CounterRoutes:
   val routes: URRoutes[CounterService] = Routes(
     Method.GET / "count" -> handler((_: Request) =>
       withContext((counterService: CounterService) =>
-        for {
-          _ <- counterService.inc
-          value <- counterService.get
-        } yield Response.text(value.toString)
+        defer:
+          counterService.inc.run
+          Response.text(counterService.get.run.toString)
       )
     )
   )
