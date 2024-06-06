db66024d2c9f7f765a64823eca08f0287fb8161a
dlbdvd96 <dlbdvd96@hotmail.it>
2024-06-06 23:43:09 +0200

    remove unused alias

--- src/main/scala/aliases/RoutesAliases.scala
+++ src/main/scala/aliases/RoutesAliases.scala
@@ -3,7 +3,6 @@ package aliases
 import zio.http.{Route, Routes}
 
 type URRoute[-Env] = Route[Env, Nothing]
-type TaskRoutes = Routes[Any, Throwable]
-type IORoutes[+Err] = Routes[Any, Err]
+
 type URoutes = Routes[Any, Nothing]
 type URRoutes[-Env] = Routes[Env, Nothing]
