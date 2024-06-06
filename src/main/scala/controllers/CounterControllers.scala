eb8b666864d58fdc00a75f5d0ed38f7b38979e10
dlbdvd96 <dlbdvd96@hotmail.it>
2024-06-06 23:37:51 +0200

    add controller utility

--- src/main/scala/controllers/CounterControllers.scala
+++ src/main/scala/controllers/CounterControllers.scala
@@ -2,12 +2,13 @@ package controllers
 
 import layers.CounterService
 import utils.Controller
+import zio.ZNothing
 import zio.http.RoutePattern.GET
 import zio.http.endpoint.Endpoint
 
 object CounterControllers:
 
-  val count = Controller(
+  val count: Controller[Unit, CounterService, ZNothing, Int] = Controller(
     Endpoint(GET / "count").out[Int],
     (counterService: CounterService) =>
       for {
--- src/main/scala/controllers/HelloControllers.scala
+++ src/main/scala/controllers/HelloControllers.scala
@@ -2,14 +2,15 @@ package controllers
 
 import layers.HelloService
 import utils.Controller
-import zio.Tag
 import zio.http.RoutePattern.POST
 import zio.http.endpoint.Endpoint
+import zio.{Tag, ZNothing}
 
 object HelloControllers:
 
-  val hello = Controller(
-    Endpoint(POST / "hello-world").in[String].out[Map[String, Int]],
-    (request: String, helloService: HelloService) =>
-      helloService.getHello(request).map(s => Map(s -> 1))
-  )
+  val hello: Controller[String, HelloService, ZNothing, Map[String, Int]] =
+    Controller(
+      Endpoint(POST / "hello-world").in[String].out[Map[String, Int]],
+      (request: String, helloService: HelloService) =>
+        helloService.getHello(request).map(s => Map(s -> 1))
+    )
